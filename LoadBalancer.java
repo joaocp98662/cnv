64a9079b346aa83618e859c40d3ebc51d8b82fa9
EC2 Default User <ec2-user@ip-172-31-81-191.ec2.internal>
2021-05-27 23:17:34 +0000

    zee

--- LoadBalancer.java
+++ LoadBalancer.java
@@ -94,12 +94,12 @@ public class LoadBalancer {
 
 		try {
 
-			String loadBalancerInstanceID = "i-0c65f95829151ddbb";
+			String loadBalancerInstanceID = "i-0e3929643c6a61e6c";
 
 			// Get imageID
 
 			Filter filterImageByName = new Filter("name");
-	        filterImageByName.withValues("CNV-Project");
+	        filterImageByName.withValues("test");
 			
         	DescribeImagesRequest describesImagesRequest = new DescribeImagesRequest();
         	describesImagesRequest.withFilters(filterImageByName);
@@ -331,7 +331,7 @@ public class LoadBalancer {
                                .withInstanceType("t2.micro")
                                .withMinCount(1)
                                .withMaxCount(1)
-                               .withKeyName("CNV-project")
+                               .withKeyName("CNV-labs-AWS")
                                .withSecurityGroups("CNV-project");
                                
             RunInstancesResult runInstancesResult = ec2.runInstances(runInstancesRequest);
